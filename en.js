eb9f3099da983334cfcdb6b7f01905506b1c0926
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-08-10 23:07:06 +0530

    Added project

---
+++ en.js
@@ -0,0 +1,8 @@
+const en  = {
+    TITLE: 'Hi',
+    FOO: 'This is a paragraph.',
+    BUTTON_LANG_EN: 'english',
+    BUTTON_LANG_DE: 'german',
+    BUTTON_LANG_FR: 'french',
+    MSG: 'Welcome User'
+}
